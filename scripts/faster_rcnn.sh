a7763f0218a9f6af98de7c45ddbb02e2382ae163
Elisa Gunnarshaug <gunnarshaug@users.noreply.github.com>
2023-02-20 13:22:46 +0100

    move scripts

--- scripts/faster_rcnn.sh
+++ scripts/faster_rcnn.sh
@@ -2,12 +2,12 @@
 #SBATCH --gres=gpu:1
 #SBATCH --partition=gpuA100 
 #SBATCH --time=02:15:00
-#SBATCH --job-name=pytorch_frcnn
+#SBATCH --job-name=frcnn
 #SBATCH --output=frcnn_train.out
  
 # Activate environment
 uenv verbose cuda-11.4 cudnn-11.4-8.2.4
 uenv miniconda-python39
-conda activate pytorch_env
+conda activate pytorch_ml_env
 # Run the Python script that uses the GPU
-python -u train.py
+python -u src/train.py
--- scripts/pytorch_env_setup.sh
+++ scripts/pytorch_env_setup.sh
@@ -2,11 +2,11 @@
 #SBATCH --gres=gpu:0
 #SBATCH --partition=gpuA100 
 #SBATCH --time=02:15:00
-#SBATCH --job-name=pytorch_faster_rcnn_setup
-#SBATCH --output=faster_rcnn_setup.out
+#SBATCH --job-name=env_setup
+#SBATCH --output=env_setup.out
  
 # Set up environment
 uenv verbose cuda-11.4 cudnn-11.4-8.2.4
 uenv miniconda-python39
-conda env create --file ../environment.yml
+conda env create --file environment.yml
 # conda create -n pytorch_env -c pytorch pytorch torchvision numpy -c conda-forge albumannotations matplotlib pandas opencv_python PyYAML -y
