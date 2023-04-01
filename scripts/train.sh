861902ca4be3e77d0245a048b523e1c58ec5b9cb
Elisa Gunnarshaug <gunnarshaug@users.noreply.github.com>
2023-04-01 18:14:29 +0200

    training scripts and configs

---
+++ scripts/train.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --gres=gpu:1
+#SBATCH --partition=gpuA100 
+#SBATCH --array=0-3
+#SBATCH --time=04:15:00
+#SBATCH --job-name=frcnn
+#SBATCH --output=frcnn_%A_%a.out
+
+uenv verbose cuda-11.4 cudnn-11.4-8.2.4
+uenv miniconda-python39
+conda activate pytorch_env
+
+configs=("frcnn_slope.yml" "frcnn_slrm.yml" "frcnn_msrm_sf1.yml" "frcnn_msrm_sf2.yml")
+config="${configs[$SLURM_ARRAY_TASK_ID]}"
+
+python train.py --config="$config"
+# python train.py --config="$config" > subtask_0${SLURM_ARRAY_TASK_ID}.txt
