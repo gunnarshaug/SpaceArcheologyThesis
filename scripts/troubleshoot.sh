2286381ee791d5916f6a15abafd08db9275cb6a4
Elisa Gunnarshaug <gunnarshaug@users.noreply.github.com>
2023-04-24 17:03:24 +0200

    troubleshooting scripts

---
+++ scripts/troubleshoot.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --gres=gpu:1
+#SBATCH --partition=gpuA100 
+#SBATCH --time=04:15:00
+#SBATCH --job-name=frcnn
+#SBATCH --nodelist=gorina8
+#SBATCH --output=frcnn.out
+
+uenv verbose cuda-11.4 cudnn-11.4-8.2.4
+uenv miniconda3-py39
+conda activate pytorch_env
+
+
+python -u src/train.py --config=troubleshoot.yml
